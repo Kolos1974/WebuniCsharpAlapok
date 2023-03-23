471d9b5fc6ea3ed6219dfa034edc9b6ad8a77cc4
Kolos <karlovitz.kolos@gmail.com>
2023-03-23 14:12:00 +0100

    HelloCSharp

---
+++ HelloCSharp/HelloCSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace HelloCSharp
+{
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var szoveg3 = "Szöveg deklarálás típus nélkül. Implicit típus megadás.";
+
+            string szoveg = "Hello C#!";
+            if (false)
+            {
+                szoveg = "Hello";
+            }
+
+            Console.WriteLine(szoveg);
+        }
+
+    }
+
+}
