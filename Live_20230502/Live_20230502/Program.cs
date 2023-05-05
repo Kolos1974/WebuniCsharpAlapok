f544b76d305a58ab95eb6a872a4b64c914708d0a
Kolos <karlovitz.kolos@gmail.com>
2023-05-05 13:38:36 +0200

    Live 20230502

---
+++ Live_20230502/Live_20230502/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Live_20230502
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FB(0, 100);
+        }
+
+        static void FB(int a, int f)
+        {
+            Enumerable.Range(a, f)
+                .ToList()
+                .ForEach(i => Console.WriteLine((i % 15 == 0) ? "FizzBuzz" : (i % 3 == 0) ? "Buzz" : (i % 5 == 0) ?
+                "Fizz" : i.ToString()));
+        }
+
+
+    }
+
+
+}
---
+++ Live_20230502_2/Live_20230502_2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Live_20230502_2
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A a1 = new A();
+            A a2 = new A();
+            a2.Kiir();
+
+        }
+
+
+    }
+
+    public class A
+    {
+        int x;
+        
+        // A static változót minden példányosított osztály látja!!
+        static int y;
+
+
+        public A()
+        {
+            Console.WriteLine("Public konstruktor");
+            x++;
+        }
+
+
+        // A static konstruktor, csak akkor fut le, egyszer, mikor az osztályból az első példányt létrehozzuk!!
+        static A()
+        {
+            Console.WriteLine("Static konstruktor");
+            y++;
+
+        }
+
+        public void Kiir()
+        {
+            Console.WriteLine($"x: {x} , y: {y}");
+        }
+
+
+    }
+
+
+
+}
