90c6754be5335e0740ea05ad57eec6702f41d508
Kolos <karlovitz.kolos@gmail.com>
2023-05-12 14:01:51 +0200

    Live_20230502_4 done

---
+++ Live_20230502_4/Live_20230502_4/Program.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+
+namespace Live_20230502_4
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Emberek e = new();
+            e.Bovit("Peti");
+            e.Bovit("Kati");
+            e.Bovit("Peti");
+
+            Console.WriteLine(e.nevek[0]);
+            Console.WriteLine("--------");
+
+
+
+            foreach (string nev in e.nevek)
+            {
+                Console.WriteLine(nev); 
+            }
+            Console.WriteLine("--------");
+
+
+            foreach (string nev in e)
+            {
+                Console.WriteLine(nev);
+            }
+            Console.WriteLine("--------");
+
+
+        }
+    }
+
+
+
+    public class Emberek : IEnumerable
+    {
+        public List<string> nevek;
+
+        public Emberek()
+        {
+            nevek = new();
+        }
+
+        public bool Hozzaad(string nev)
+        {
+            if (nevek.Contains(nev))
+            {
+                return false;
+            }
+            nevek.Add(nev);
+            return true;
+
+        }
+
+        public void Bovit(string nev)
+        {
+            if (Hozzaad(nev))
+            {
+                Console.WriteLine($"{nev} - Sikeres hozzáadás");
+            }
+            else
+            {
+                Console.WriteLine($"{nev} - Sikertelen hozzáadás");
+            }
+
+
+
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new EmberEnumerator(nevek);
+        }
+    }
+
+
+    public class EmberEnumerator : IEnumerator
+    {
+        List<string> lokalisadatok;
+        int i = -1;
+
+        public EmberEnumerator(List<string> adatok)
+        {
+            lokalisadatok = adatok;
+        }
+
+
+        public object Current => lokalisadatok[i];
+
+        public bool MoveNext()
+        {
+            if (i < lokalisadatok.Count - 1)
+            {
+                i++;
+                return true;
+            }
+            else
+            { return false; }
+
+        }
+
+        public void Reset()
+        {
+            i = -1;
+        }
+    }
+
+}
+
---
+++ Live_20230502_4/Live_20230502_4B/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+
+namespace Live_20230502_4B
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Auto> autok = new List<Auto>();
+
+            autok.Add(new Auto("AAA-123", "Ford"));
+            autok.Add(new Auto("BBB-123", "Porsche"));
+
+            foreach (Auto auto in autok)
+            {
+                Console.WriteLine(auto.PlateNumber + " - " + auto.Brand);
+                //Console.WriteLine(auto);
+
+            }
+
+
+            Console.WriteLine("--------");
+
+            Jarmuvek jm = new Jarmuvek();
+            jm.addAuto(new Auto("AAA-123", "Ford"));
+            jm.addAuto(new Auto("BBB-123", "Porsche"));
+            foreach (Auto a in jm)
+            {
+                Console.WriteLine(a.PlateNumber + " - "+ a.Brand);
+            }
+
+
+        }
+    }
+
+
+    public class Auto
+    {
+        public String PlateNumber { get; set; }
+        public String Brand { get; set; }
+
+        public Auto(String PlateNumber, String Brand)
+        {
+            this.PlateNumber = PlateNumber;
+            this.Brand = Brand;
+        }
+
+    }
+
+    public class Jarmuvek : IEnumerable
+    {
+        List<Auto> autok;
+
+        public Jarmuvek()
+        {
+            autok = new();
+        }
+
+
+        public void addAuto(Auto auto)
+        {
+            autok.Add(auto);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new JarmuvekEnumerator(autok);
+        }
+    }
+
+
+    public class JarmuvekEnumerator : IEnumerator
+    {
+        List<Auto> localAutok;
+        int i = -1;
+
+        public JarmuvekEnumerator(List<Auto> autok)
+        {
+            localAutok = autok;
+        }
+
+
+        public object Current => localAutok[i];
+
+        public bool MoveNext()
+        {
+            if (i < localAutok.Count - 1)
+            {
+                i++;
+                return true;
+            }
+            else
+            { return false; }
+        }
+
+        public void Reset()
+        {
+            i = -1;
+        }
+    }
+
+
+
+
+
+
+}
+
