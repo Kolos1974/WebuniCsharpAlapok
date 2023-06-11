f65572bbc995c1a226d403d47ac99bb004f6f066
Kolos1974 <karlovitz.kolos@gmail.com>
2023-06-11 18:22:05 +0200

    DemoWPF

---
+++ DemoWPF/DemoWPF/Ember.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoWPF
+{
+    public  class Ember
+    {
+
+        public string Nev { get; set; }
+
+        public int Szev { get; set; }
+
+        public Ember(string n, int s)
+        {
+            this.Nev = n;
+            this.Szev = s;
+            
+        }
+        /*
+        public override string ToString()
+        {
+            return $"{Nev} - {Szev}";
+        }
+        */
+
+    }
+
+    public class Embers
+    {
+        public ObservableCollection<Ember> Emberek { get; set; }
+
+
+        public Embers()
+        {
+            Emberek = new ObservableCollection<Ember>()
+            {
+                new Ember("Peti", 1999),
+                new Ember("KAti", 2022),
+                new Ember("Laci", 2001)
+
+            };
+
+
+
+
+        }
+    }
+
+}
